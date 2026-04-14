1da0c3bf53b60aceed287cfe8bbb62e5d764fd85
EthanVo <vvotandat78@gmail.com>
2026-04-14 19:14:31 +0700

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
-import { LandingPage } from "./ui/landingpage";
+import { LandingPage } from "./ui/landingpage.tsx";
 import "@mysten/dapp-kit/dist/index.css";
 
 const queryClient = new QueryClient();
